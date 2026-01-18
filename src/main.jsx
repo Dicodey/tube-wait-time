127c82d38a8ce9288f398d4797013ad0d70c318a
Di <diderik.ypma@live.co.uk>
2026-01-18 01:03:45 +0000

    Update app

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
 // Register Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/tube-wait-time/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(reg => console.log('SW registered'))
       .catch(err => console.log('SW registration failed', err));
   });
